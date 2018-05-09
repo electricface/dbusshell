8758a283c722e8ef12e25dd80d254c165979e18f
electricface <songwentai@deepin.com>
2018-05-09 15:01:58 +0800

    init

---
+++ main.go
@@ -0,0 +1,484 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"encoding/xml"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	"github.com/abiosoft/ishell"
+	ofdbus "github.com/linuxdeepin/go-dbus-factory/org.freedesktop.dbus"
+	"pkg.deepin.io/lib/dbus1"
+	"pkg.deepin.io/lib/dbus1/introspect"
+)
+
+var conn *dbus.Conn
+var connDesc string
+var dbusName string
+var dbusPath = "/"
+var dbusInterface string
+var logger *log.Logger
+var logFile string
+
+func init() {
+	logFile = fmt.Sprintf("/tmp/dbusshell-%d", os.Getpid())
+	f, err := os.Create(logFile)
+	if err != nil {
+		panic(err)
+	}
+
+	logger = log.New(f, "", log.Lshortfile)
+}
+
+func fixArgsName(args []introspect.Arg) {
+	if len(args) > 0 {
+		if args[0].Name == "" {
+			for idx := range args {
+				args[idx].Name = "arg" + strconv.Itoa(idx)
+			}
+		}
+	}
+}
+
+func getMethodArgsString(args []introspect.Arg) string {
+	var inArgs []string
+	var outArgs []string
+
+	fixArgsName(args)
+	for _, arg := range args {
+		if arg.Direction == "in" {
+			inArgs = append(inArgs, arg.Name+" "+arg.Type)
+		} else if arg.Direction == "out" {
+			outArgs = append(outArgs, arg.Name+" "+arg.Type)
+		}
+	}
+	return fmt.Sprintf("(%s) -> (%s)",
+		strings.Join(inArgs, ", "),
+		strings.Join(outArgs, ", "))
+}
+
+func getSignalArgsString(args []introspect.Arg) string {
+	var argStrv []string
+	fixArgsName(args)
+	for _, arg := range args {
+		argStrv = append(argStrv, arg.Name+" "+arg.Type)
+	}
+	return fmt.Sprintf("(%s)", strings.Join(argStrv, ", "))
+}
+
+func showInterface(ctx *ishell.Context, ifc introspect.Interface) {
+	var buf bytes.Buffer
+	buf.WriteString("interface: " + ifc.Name + "\n")
+
+	if len(ifc.Methods) > 0 {
+		buf.WriteString(" methods:\n")
+		for _, method := range ifc.Methods {
+			argsStr := getMethodArgsString(method.Args)
+			buf.WriteString("  " + method.Name + argsStr + "\n")
+		}
+	}
+
+	if len(ifc.Properties) > 0 {
+		buf.WriteString(" Properties:\n")
+		for _, prop := range ifc.Properties {
+			buf.WriteString(fmt.Sprintf("  %s %s %s\n", prop.Name, prop.Type, prop.Access))
+		}
+	}
+
+	if len(ifc.Signals) > 0 {
+		buf.WriteString(" Signals:\n")
+		for _, signal := range ifc.Signals {
+			argsStr := getSignalArgsString(signal.Args)
+			buf.WriteString("  " + signal.Name + argsStr + "\n")
+		}
+	}
+	ctx.Printf("%s", buf.Bytes())
+}
+
+func main() {
+	shell := ishell.New()
+	shell.Println("dbus shell")
+
+	shell.AddCmd(&ishell.Cmd{
+		Name: "conn",
+		Func: func(c *ishell.Context) {
+			if len(c.Args) == 1 {
+				arg0 := c.Args[0]
+				if arg0 == "session" || arg0 == "e" {
+					var err error
+					conn, err = dbus.SessionBus()
+					if err != nil {
+						c.Err(err)
+						return
+					}
+					connDesc = "session bus"
+				} else if arg0 == "system" || arg0 == "y" {
+					var err error
+					conn, err = dbus.SystemBus()
+					if err != nil {
+						c.Err(err)
+						return
+					}
+					connDesc = "system bus"
+				}
+			}
+
+			if conn != nil {
+				c.Println(connDesc)
+				c.Printf("names: %v\n", conn.Names())
+			} else {
+				c.Println("conn is nil")
+			}
+		},
+	})
+
+	shell.AddCmd(&ishell.Cmd{
+		Name: "ls_services",
+		Func: func(c *ishell.Context) {
+			if conn == nil {
+				c.Println("conn is nil")
+				return
+			}
+
+			ofd := ofdbus.NewDBus(conn)
+			names, err := ofd.ListNames(0)
+			if err != nil {
+				c.Err(err)
+				return
+			}
+
+			var buf bytes.Buffer
+			for _, name := range names {
+				if !strings.HasPrefix(name, ":") {
+					buf.WriteString(name)
+					buf.WriteByte('\n')
+				}
+			}
+			c.ShowPaged(buf.String())
+		},
+	})
+
+	shell.AddCmd(&ishell.Cmd{
+		Name:    "change_service",
+		Aliases: []string{"cs"},
+		Func: func(c *ishell.Context) {
+			if len(c.Args) == 1 {
+				dbusName = c.Args[0]
+				dbusPath = "/"
+			}
+			c.Printf("dbusName: %q\n", dbusName)
+		},
+		Completer: func(args []string) []string {
+			return getServices()
+		},
+	})
+
+	shell.AddCmd(&ishell.Cmd{
+		Name: "ls",
+		Func: func(c *ishell.Context) {
+			node, err := getNode()
+			if err != nil {
+				c.Err(err)
+				return
+			}
+
+			for _, ifc := range node.Interfaces {
+				ifcName := ifc.Name
+				if ifc.Name == dbusInterface {
+					ifcName += "*"
+				}
+				c.Println(ifcName)
+			}
+			for _, child := range node.Children {
+				c.Println(child.Name + "/")
+			}
+		},
+	})
+
+	shell.AddCmd(&ishell.Cmd{
+		Name: "show",
+		Func: func(c *ishell.Context) {
+			if dbusInterface == "" {
+				return
+			}
+
+			node, err := getNode()
+			if err != nil {
+				c.Err(err)
+				return
+			}
+
+			for _, ifc := range node.Interfaces {
+				if ifc.Name == dbusInterface {
+					showInterface(c, ifc)
+				}
+			}
+		},
+	})
+
+	shell.AddCmd(&ishell.Cmd{
+		Name: "cd",
+		Func: func(c *ishell.Context) {
+			newPath := getNewPath(c.Args)
+
+			node, err := getNodeWithPath(newPath)
+			if err != nil {
+				c.Err(err)
+				return
+			}
+
+			if len(node.Interfaces)+len(node.Children) == 0 {
+				c.Println("failed to cd to", newPath)
+				return
+			}
+
+			c.Println("cd to", newPath)
+			dbusPath = newPath
+		},
+		Completer: func(args []string) (result []string) {
+			logger.Println("cd completer args:", args)
+
+			newPath := getNewPath(args)
+			logger.Println("newPath:", newPath)
+			node, err := getNodeWithPath(newPath)
+			if err != nil {
+				return nil
+			}
+
+			for _, child := range node.Children {
+				result = append(result, child.Name)
+			}
+			return
+		},
+	})
+
+	shell.AddCmd(&ishell.Cmd{
+		Name: "get",
+		Func: func(c *ishell.Context) {
+			if len(c.Args) != 1 {
+				return
+			}
+
+			if conn == nil {
+				c.Println("conn is nil")
+				return
+			}
+
+			arg0 := c.Args[0]
+			obj := conn.Object(dbusName, dbus.ObjectPath(dbusPath))
+			var variant dbus.Variant
+			err := obj.Call("org.freedesktop.DBus.Properties.Get", 0,
+				dbusInterface, arg0).Store(&variant)
+			if err != nil {
+				c.Err(err)
+				return
+			}
+			c.Println(formatVariant(variant))
+		},
+		Completer: func(args []string) (result []string) {
+			ifc, err := getInterface()
+			if err != nil {
+				return nil
+			}
+
+			for _, prop := range ifc.Properties {
+				result = append(result, prop.Name)
+			}
+			return
+		},
+	})
+
+	shell.AddCmd(&ishell.Cmd{
+		Name: "get_all",
+		Func: func(c *ishell.Context) {
+			if conn == nil {
+				c.Println("conn is nil")
+				return
+			}
+
+			obj := conn.Object(dbusName, dbus.ObjectPath(dbusPath))
+			var props map[string]dbus.Variant
+			err := obj.Call("org.freedesktop.DBus.Properties.GetAll", 0,
+				dbusInterface).Store(&props)
+			if err != nil {
+				c.Err(err)
+				return
+			}
+			for propName, propValue := range props {
+				c.Print(propName + ": ")
+				c.Println(formatVariant(propValue))
+			}
+		},
+	})
+
+	shell.AddCmd(&ishell.Cmd{
+		Name: "pwd",
+		Func: func(c *ishell.Context) {
+			c.Println(dbusPath)
+		},
+	})
+
+	shell.AddCmd(&ishell.Cmd{
+		Name: "info",
+		Func: func(c *ishell.Context) {
+			c.Println(connDesc)
+			c.Println("service:", dbusName)
+			c.Println("path:", dbusPath)
+			c.Println("interface:", dbusInterface)
+			c.Println("pid:", os.Getpid())
+		},
+	})
+
+	shell.AddCmd(&ishell.Cmd{
+		Name:    "interface",
+		Aliases: []string{"ifc"},
+		Func: func(c *ishell.Context) {
+			if len(c.Args) == 1 {
+				arg0 := c.Args[0]
+				dbusInterface = arg0
+				c.Println("select interface:", arg0)
+			}
+		},
+		Completer: func(args []string) []string {
+			return getInterfaces()
+		},
+	})
+
+	shell.AddCmd(&ishell.Cmd{
+		Name: "help_types",
+		Func: func(c *ishell.Context) {
+			c.Print(`Base Types:
+bool b
+byte y
+int16 n uint16 q
+int32 i uint32 u
+int64 x uint64 t
+double d
+unixFd h
+
+as -> []string
+a{ss} -> map[string]string
+(is) struct{int32,string}
+`)
+		},
+	})
+
+	shell.Run()
+	shell.Close()
+
+	// remove log file
+	err := os.Remove(logFile)
+	if err != nil {
+		log.Printf("warning: failed to remove logFile %q: %v\n", logFile, err)
+	}
+}
+
+func getNewPath(args []string) string {
+	var newPath string
+
+	if len(args) == 1 && strings.HasPrefix(args[0], "/") {
+		newPath = args[0]
+	} else {
+		joinArgs := append([]string{dbusPath}, args...)
+		newPath = filepath.Join(joinArgs...)
+	}
+
+	newPath = filepath.Clean(newPath)
+	return newPath
+}
+
+func formatVariant(variant dbus.Variant) string {
+	val := variant.Value()
+	switch v := val.(type) {
+	case string:
+		// is json?
+		data := []byte(v)
+		if json.Valid(data) {
+			var buf bytes.Buffer
+			json.Indent(&buf, data, "", "  ")
+			return fmt.Sprintf("%s", buf.Bytes())
+		} else {
+			return v
+		}
+	default:
+		return fmt.Sprintf("%#v", val)
+	}
+}
+
+func getInterface() (*introspect.Interface, error) {
+	node, err := getNode()
+	if err != nil {
+		return nil, err
+	}
+
+	if dbusInterface == "" {
+		return nil, errors.New("dbusInterface is empty")
+	}
+
+	for _, ifc := range node.Interfaces {
+		if ifc.Name == dbusInterface {
+			return &ifc, nil
+		}
+	}
+	return nil, errors.New("not found interface")
+}
+
+func getNode() (*introspect.Node, error) {
+	return getNodeWithPath(dbusPath)
+}
+
+func getNodeWithPath(path string) (*introspect.Node, error) {
+	if conn == nil {
+		return nil, errors.New("conn is nil")
+	}
+	obj := conn.Object(dbusName, dbus.ObjectPath(path))
+	var xmlStr string
+	err := obj.Call("org.freedesktop.DBus.Introspectable.Introspect",
+		0).Store(&xmlStr)
+	if err != nil {
+		return nil, err
+	}
+	var node introspect.Node
+	err = xml.Unmarshal([]byte(xmlStr), &node)
+	if err != nil {
+		return nil, err
+	}
+	return &node, nil
+}
+
+func getInterfaces() (result []string) {
+	node, err := getNode()
+	if err != nil {
+		return nil
+	}
+
+	for _, ifc := range node.Interfaces {
+		result = append(result, ifc.Name)
+	}
+	return
+}
+
+func getServices() (result []string) {
+	if conn == nil {
+		return nil
+	}
+
+	ofd := ofdbus.NewDBus(conn)
+	names, err := ofd.ListNames(0)
+	if err != nil {
+		return
+	}
+
+	for _, name := range names {
+		if !strings.HasPrefix(name, ":") {
+			result = append(result, name)
+		}
+	}
+	return
+}
